b92b873cb41687504dfbd1ffbdcf7c8c65c2850f
thompsonslee <thompson.slee@hotmail.co.uk>
2024-06-07 16:25:59 +1000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express, {Express, Request, Response, NextFunction, ErrorRequestHandler} from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import router from "./routes/apiRouter"
+
+dotenv.config()
+
+
+const port = 3000;
+
+const mongoDB_URI = process.env.mongoDB_URI
+
+async function mongoDB_connect(){
+    if(!mongoDB_URI){
+        console.log("No mongoDB URI")
+        return
+    }
+    await mongoose.connect(mongoDB_URI).catch((e) => console.log(e))
+}
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+  });
+
+mongoDB_connect()
+
+const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log("an error occured")
+    res.status(500).send(error.message)
+}
+
+const app = express();
+
+app.use("/", router)
+
+app.use(errorHandler)
+
+app.listen(port,() =>`Server is running at http://localhost:${port}`)
