0f40d5273b1fa79458aae9074e0d0da4191fd29f
thompsonslee <thompson.slee@hotmail.co.uk>
2024-06-25 16:37:17 +1000

    change relation between area and productInstances

--- src/models/Area.ts
+++ src/models/Area.ts
@@ -2,12 +2,10 @@ import { model, Schema, Document, ObjectId } from "mongoose";
 
 export interface Area extends Document{
     user: ObjectId,
-    products: Array<ObjectId>
 }
 
 const AreaSchema = new Schema({
     user: {type: Schema.ObjectId, ref: "User"},
-    products: [{type:Schema.ObjectId, ref:"ProductInstances" }]
 })
 
 const Area = model<Area>("Inventory",AreaSchema)
--- src/models/ProductInstances.ts
+++ src/models/ProductInstances.ts
@@ -1,12 +1,14 @@
 import { model, Schema, Document, ObjectId} from "mongoose";
 
 export interface ProductInstance extends Document{
-    Product: ObjectId
+    product: ObjectId
+    area: ObjectId
     quantity: number
 }
 
 const ProductInstanceSchema = new Schema({
     product: {type: Schema.ObjectId, ref: "Product"},
+    area: {type: Schema.ObjectId, ref: "Area"},
     quantity: {type: Number}
 })
 
